41147507e388e6e5ce3429e0042412b4cd7efd08
Kevin Lewis <49124435+klewis0555@users.noreply.github.com>
2026-03-17 10:33:00 -0500

    start references file

---
+++ references.py
@@ -0,0 +1,8 @@
+class Item:
+  def __init__(self, name, description):
+    self.name = name
+    self.description = description
+
+ITEMS = [
+  Item("Premium Bedroll", "during downtime, you automatically clear a Stress.")
+]
