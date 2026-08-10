921a65adaba937b615c799361a81c26e52d84de0
Kevin Lewis <kluv0555@gmail.com>
2026-08-10 21:42:49 +0100

    Equipment and Items references from SRD and Hope & Fear expansion.

---
+++ consumables.py
@@ -0,0 +1,73 @@
+class Consumable:
+  def __init__(self, name, description):
+    self.name = name
+    self.description = description
+
+  def __str__(self):
+    return self.name
+
+  def __repr__(self):
+    return self.name
+
+SRD_CONSUMABLES = [
+  Consumable("Stride Potion", "You gain a +1 bonus to your next Agility Roll."),
+  Consumable("Bolster Potion", "You gain a +1 bonus to your next Strength Roll."),
+  Consumable("Control Potion", "You gain a +1 bonus to your next Finesse Roll."),
+  Consumable("Attune Potion", "You gain a +1 bonus to your next Instinct Roll."),
+  Consumable("Charm Potion", "You gain a +1 bonus to your next Presence Roll."),
+  Consumable("Enlighten Potion", "You gain a +1 bonus to your next Knowledge Roll."),
+  Consumable("Minor Health Potion", "Clear 1d4 HP."),
+  Consumable("Minor Stamina Potion", "Clear 1d4 Stress."),
+  Consumable("Grindeltooth Venom", "You can apply this venom to a weapon that deals physical damage to add a d6 to your next damage roll with that weapon."),
+  Consumable("Varik Leaves", "You can eat these paired leaves to immediately gain 2 Hope."),
+  Consumable("Vial of Moondrip", "When you drink the contents of this vial, you can see in total darkness until your next rest."),
+  Consumable("Unstable Arcane Shard", "You can make a Finesse Roll to throw this shard at a group of adversaries within Far range. Targets you succeed against take 1d20 magic damage."),
+  Consumable("Potion of Stability", "You can drink this potion to choose one additional downtime move."),
+  Consumable("Improved Grindeltooth Venom", "You can apply this venom to a weapon that deals physical damage to add a d8 to your next damage roll with that weapon."),
+  Consumable("Morphing Clay", "You can spend 4 Hope to use this clay, altering your face enough to make you unrecognizable until your next rest."),
+  Consumable("Vial of Darksmoke", "When an adversary attacks you, use this vial and roll a number of d6 equal to your Agility. Add the highest result to your Evasion against the attack."),
+  Consumable("Jumping Root", "Eat this root to leap up to Far range once without needing to roll."),
+  Consumable("Snap Powder", "Mark a Stress and clear a HP."),
+  Consumable("Health Potion", "Clear 1d4+1 HP."),
+  Consumable("Stamina Potion", "Clear 1d4+1 Stress."),
+  Consumable("Armor Stitcher", "You can use this stitcher to spend any number of Hope and clear that many Armor Slots."),
+  Consumable("Gill Salve", "You can apply this salve to your neck to breathe underwater for a number of minutes equal to your level."),
+  Consumable("Replication Parchment", "By touching this piece of parchment to another, you can perfectly copy the second parchment's contents. Once used, this parchment becomes mundane paper."),
+  Consumable("Improved Arcane Shard", "You can make a Finesse Roll to throw this shard at a group of adversaries within Far range. Targets you succeed against take 2d20 magic damage."),
+  Consumable("Major Stride Potion", "You gain a +1 bonus to your Agility until your next rest."),
+  Consumable("Major Bolster Potion", "You gain a +1 bonus to your Strength until your next rest."),
+  Consumable("Major Control Potion", "You gain a +1 bonus to your Finesse until your next rest."),
+  Consumable("Major Attune Potion", "You gain a +1 bonus to your Instinct until your next rest."),
+  Consumable("Major Charm Potion", "You gain a +1 bonus to your Presence until your next rest."),
+  Consumable("Major Enlighten Potion", "You gain a +1 bonus to your Knowledge until your next rest."),
+  Consumable("Blood of the Yorgi", "You can drink this blood to disappear from where you are and immediately reappear at a point you can see within very Far range."),
+  Consumable("Hornet's Secret Potion", "After drinking this potion, the next successful attack you make critically succeeds."),
+  Consumable("Redthorn Saliva", "You can apply this saliva to a weapon that deals physical damage to add a d12 to your next damage roll with that weapon."),
+  Consumable("Channelstone", "You can use this stone to take a spell or grimoire from your vault, use it once, and return it to your vault."),
+  Consumable("Mythic Dust", "You can apply this dust to a weapon that deals magic damage to add a d12 to your next damage roll with that weapon."),
+  Consumable("Acidpaste", "This paste eats away walls and other surfaces in bright flashes."),
+  Consumable("Hopehold Flare", "When you use this flare, allies within Close range roll a d6 when they spend a Hope. On a result of 6, they gain the effect of that Hope without spending it. The flare lasts until the end of the scene."),
+  Consumable("Major Arcane Shard", "You can make a Finesse Roll to throw this shard at a group of adversaries within Far range. Targets you succeed against take 4d20 magic damage."),
+  Consumable("Featherbone", "You can use this bone to control your falling speed for a number of minutes equal to your level."),
+  Consumable("Circle of the Void", "Mark a Stress to create a void that extends up to Far range. No magic can be cast inside the void, and creatures within the void are immune to magic damage."),
+  Consumable("Sun Tree Sap", "Consume this sap to roll a d6. On a result of 5-6, clear 2 HP. On a result of 2-4, clear 3 Stress. On a result of 1, see through the veil of death and return unscathed, gaining one scar."),
+  Consumable("Dripfang Poison", "A creature who consumes this poison takes 2d10 direct magic damage."),
+  Consumable("Major Health Potion", "Clear 1d4+2 HP."),
+  Consumable("Major Stamina Potion", "Clear 1d4+2 Stress."),
+  Consumable("Ogre Musk", "You can use this musk to prevent anyone from tracking you by mundane or magical means until your next rest."),
+  Consumable("Wingsprout", "You gain magic wings that allow you to fly for a number of minutes equal to your level."),
+  Consumable("Jar of Lost Voices", "You can open this jar to release a deafening echo of voices for a number of minutes equal to your Instinct. Creatures within Far range unprepared for the sound take 6d8 magic damage."),
+  Consumable("Dragonbloom Tea", "You can drink this tea to unleash a fiery breath attack. Make an Instinct Roll against all adversaries in front of you within Close range. Targets you succeed against take 2d20 physical damage using your Proficiency."),
+  Consumable("Bridge Seed", "Thick vines grow from your location to a point of your choice within Far range, allowing you to climb up or across them. The vines dissipate on your next short rest."),
+  Consumable("Sleeping Sap", "You can drink this potion to fall asleep for a full night's rest. You clear all Stress upon waking."),
+  Consumable("Feast of Xurla", "You can eat this meal to clear all HP and Stress and gain 1d4 Hope."),
+  Consumable("Bonding Honey", "This honey can be used to glue two objects together permanently."),
+  Consumable("Shrinking Potion", "You can drink this potion to halve your size until you choose to drop this form or your next rest. While in this form, you have a +2 bonus to Agility and a -1 penalty to your Proficiency."),
+  Consumable("Growing Potion", "You can drink this potion to double your size until you choose to drop this form or your next rest. While in this form, you have a +2 bonus to Strength and a +1 bonus to your Proficiency."),
+  Consumable("Knowledge Stone", "If you die while holding this stone, an ally can take a card from your loadout to place in their loadout or vault. After they take this knowledge, the stone crumbles."),
+  Consumable("Sweet Moss", "You can consume this moss during a rest to clear 1d10 HP or 1d10 Stress."),
+  Consumable("Blinding Orb", "You can activate this orb to create a flash of bright light. All targets within Close range become Blinded until they mark HP."),
+  Consumable("Death Tea", "After you drink this tea, you instantly kill your target when you critically succeed on an attack. If you don't critically succeed on an attack before your next long rest, you die."),
+  Consumable("Mirror of Marigold", "When you take damage, you can spend a Hope to negate that damage, after which the mirror shatters."),
+  Consumable("Stardrop", "You can use this stardrop to summon a hailstorm of comets that deals 8d20 physical damage to all targets within Very Far range."),
+]
--- generate_references_from_csv.py
+++ generate_references_from_csv.py
@@ -1,26 +1,50 @@
 # This file can be run to regenerate the records in weapons.py from a csv.
 import csv
 
-data = []
+weapon_data = []
 with open('csv_data/tier1-weapons.csv', mode='r', encoding='utf-8') as file:
   csv_data = csv.DictReader(file)
   for row in csv_data:
-    data.append('Weapon("' + '", "'.join(list(row.values())) + '", 1)')
+    weapon_data.append('Weapon("' + '", "'.join(list(row.values())) + '", 1)')
 
 with open('csv_data/tier2-weapons.csv', mode='r', encoding='utf-8') as file:
   csv_data = csv.DictReader(file)
   for row in csv_data:
-    data.append('Weapon("' + '", "'.join(list(row.values())) + '", 2)')
+    weapon_data.append('Weapon("' + '", "'.join(list(row.values())) + '", 2)')
 
 with open('csv_data/tier3-weapons.csv', mode='r', encoding='utf-8') as file:
   csv_data = csv.DictReader(file)
   for row in csv_data:
-    data.append('Weapon("' + '", "'.join(list(row.values())) + '", 3)')
+    weapon_data.append('Weapon("' + '", "'.join(list(row.values())) + '", 3)')
 
 with open('csv_data/tier4-weapons.csv', mode='r', encoding='utf-8') as file:
   csv_data = csv.DictReader(file)
   for row in csv_data:
-    data.append('Weapon("' + '", "'.join(list(row.values())) + '", 4)')
+    weapon_data.append('Weapon("' + '", "'.join(list(row.values())) + '", 4)')
+
+armor_data = []
+with open('csv_data/armor.csv', mode='r', encoding='utf-8') as file:
+  csv_data = csv.DictReader(file)
+  for row in csv_data:
+    armor_data.append('Armor("' + '", "'.join(list(row.values())) + '")')
+
+srd_items = []
+with open('csv_data/srd_items.csv', mode='r', encoding='utf-8') as file:
+  csv_data = csv.DictReader(file)
+  for row in csv_data:
+    srd_items.append('Item("' + '", "'.join(list(row.values())) + '")')
+
+hf_items = []
+with open('csv_data/hf_items.csv', mode='r', encoding='utf-8') as file:
+  csv_data = csv.DictReader(file)
+  for row in csv_data:
+    hf_items.append('Item("' + '", "'.join(list(row.values())) + '")')
+
+srd_consumables = []
+with open('csv_data/srd_consumables.csv', mode='r', encoding='utf-8') as file:
+  csv_data = csv.DictReader(file)
+  for row in csv_data:
+    srd_consumables.append('Consumable("' + '", "'.join(list(row.values())) + '")')
 
 with open('weapons.py', mode='w') as file:
   file.write('class Weapon:\n')
@@ -39,6 +63,62 @@ with open('weapons.py', mode='w') as file:
   file.write('    return self.name\n')
   file.write('\n')
   file.write('WEAPONS = [\n')
-  for row in data:
+  for row in weapon_data:
+    file.write('  ' + row + ',\n')
+  file.write(']\n')
+
+with open('armor.py', mode='w') as file:
+  file.write('class Armor:\n')
+  file.write('  def __init__(self, name, tier, base_thresholds, score, feature):\n')
+  file.write('    self.name = name\n')
+  file.write('    self.tier = tier\n')
+  file.write('    self.base_thresholds = base_thresholds\n')
+  file.write('    self.score = score\n')
+  file.write('    self.feature = feature\n')
+  file.write('  def __str__(self):\n')
+  file.write('    return self.name\n')
+  file.write('\n')
+  file.write('ARMOR = [\n')
+  for row in armor_data:
+    file.write('  ' + row + ',\n')
+  file.write(']\n')
+
+with open('items.py', mode='w') as file:
+  file.write('class Item:\n')
+  file.write('  def __init__(self, name, description):\n')
+  file.write('    self.name = name\n')
+  file.write('    self.description = description\n')
+  file.write('\n')
+  file.write('  def __str__(self):\n')
+  file.write('    return self.name\n')
+  file.write('\n')
+  file.write('  def __repr__(self):\n')
+  file.write('    return self.name\n')
+  file.write('\n')
+  file.write('SRD_ITEMS = [\n')
+  for row in srd_items:
+    file.write('  ' + row + ',\n')
+  file.write(']\n')
+  file.write('\n')
+  file.write('HF_ITEMS = [\n')
+  for row in hf_items:
+    file.write('  ' + row + ',\n')
+  file.write(']\n')
+
+
+with open('consumables.py', mode='w') as file:
+  file.write('class Consumable:\n')
+  file.write('  def __init__(self, name, description):\n')
+  file.write('    self.name = name\n')
+  file.write('    self.description = description\n')
+  file.write('\n')
+  file.write('  def __str__(self):\n')
+  file.write('    return self.name\n')
+  file.write('\n')
+  file.write('  def __repr__(self):\n')
+  file.write('    return self.name\n')
+  file.write('\n')
+  file.write('SRD_CONSUMABLES = [\n')
+  for row in srd_consumables:
     file.write('  ' + row + ',\n')
   file.write(']\n')
